c56e2972a74ee979ca852bf57e4f2a4fd1326187
avi <avi@kali.avi.com>
2020-06-22 21:43:34 +0530

    first commit

---
+++ Colors.js
@@ -0,0 +1,8 @@
+const colors = {
+  black: "#2D3436",
+  blue: "#248fa6",
+  lightBlue: "#A7CBD9",
+  white: "#FFFFFF",
+};
+
+export default colors;
---
+++ tempData.js
@@ -0,0 +1,64 @@
+// temporary Data in the App
+
+const tempData = [
+  {
+    name: "Plan a Trip",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Book flight",
+        completed: false,
+      },
+      {
+        title: "Passport Check",
+        completed: true,
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: true,
+      },
+      {
+        title: "Pack Luggage",
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: "Learn React",
+    color: "#00cec9",
+    todos: [
+      {
+        title: "Basics",
+        completed: true,
+      },
+      {
+        title: "Hooks",
+        completed: false,
+      },
+      {
+        title: "Redux",
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: "Learn React",
+    color: "#00cec9",
+    todos: [
+      {
+        title: "Basics",
+        completed: true,
+      },
+      {
+        title: "Hooks",
+        completed: false,
+      },
+      {
+        title: "Redux",
+        completed: false,
+      },
+    ],
+  },
+];
+
+export default tempData;
